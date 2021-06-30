df0dde00bb828ce290e1edb23d87bc785cabf6c7
ShamReh <sr.166@hotmail.co.uk>
2021-06-30 12:17:48 +0100

    Calculator

---
+++ calculator/Calculator.java
@@ -0,0 +1,26 @@
+package calculator;
+
+public class Calculator {
+
+	public static int add(int a, int b) {
+		int add = a + b;
+		return add;
+
+	}
+
+	public static int subtract(int a, int b) {
+		int subtract = a - b;
+		return subtract;
+
+	}
+
+	public static int multiply(int a, int b) {
+		int multiply = a * b;
+		return multiply;
+	}
+
+	public static double divide(double a, double b) {
+		double divide = a / b;
+		return divide;
+	}
+}
---
+++ calculatortest/Test.java
@@ -0,0 +1,15 @@
+package calculatortest;
+
+import calculator.Calculator;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(Calculator.add(5, 10));
+		System.out.println(Calculator.subtract(18, 5));
+		System.out.println(Calculator.multiply(5, 15));
+		System.out.println(Calculator.divide(5, 30));
+
+	}
+
+}
